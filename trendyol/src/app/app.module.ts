30cf16b6688696b5d82c0d5018c59de242b7e41b
serhasHacizade <eren.felek.4@gmail.com>
2024-07-10 14:53:37 +0300

    commit

--- trendyol/src/app/app.module.ts
+++ trendyol/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+;
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarModule } from './navbar/navbar.module';
 
@@ -16,7 +18,7 @@ import { NavbarModule } from './navbar/navbar.module';
     BrowserModule,
     AppRoutingModule,
     NavbarModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- trendyol/src/app/navbar/navbar.module.ts
+++ trendyol/src/app/navbar/navbar.module.ts
@@ -2,19 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { TopListComponent } from './top-list/top-list.component';
-
-
+import { HeaderComponent } from './header/header.component';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  heroMagnifyingGlass,
+  heroUser,
+  heroHeart,
+  heroShoppingCart,
+} from '@ng-icons/heroicons/outline';
+import {
+  heroUserSolid,
+  heroHeartSolid,
+  heroShoppingCartSolid,
+} from '@ng-icons/heroicons/solid';
 
 @NgModule({
-  declarations: [
-    NavbarComponent,
-    TopListComponent
-  ],
+  declarations: [NavbarComponent, TopListComponent, HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgIconsModule.withIcons({
+      heroMagnifyingGlass,
+      heroUser,
+      heroHeart,
+      heroShoppingCart,
+      heroUserSolid,
+      heroHeartSolid,
+      heroShoppingCartSolid,
+    }),
   ],
-  exports: [
-    NavbarComponent
-  ]
+  exports: [NavbarComponent],
 })
-export class NavbarModule { }
+export class NavbarModule {}
