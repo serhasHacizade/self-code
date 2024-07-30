7101e2b0372acd422953a6ee6152230702afbd2a
serhasHacizade <eren.felek.4@gmail.com>
2024-07-30 11:58:23 +0300

    commit

--- trendyol/src/app/components/footer/footer.module.ts
+++ trendyol/src/app/components/footer/footer.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
 
-
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  bootstrapFacebook,
+  bootstrapInstagram,
+  bootstrapTwitterX,
+  bootstrapYoutube,
+} from '@ng-icons/bootstrap-icons';
 
 @NgModule({
-  declarations: [
-    FooterComponent
-  ],
+  declarations: [FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgIconsModule.withIcons({
+      bootstrapFacebook,
+      bootstrapInstagram,
+      bootstrapTwitterX,
+      bootstrapYoutube,
+    }),
   ],
   exports: [FooterComponent],
 })
-export class FooterModule { }
+export class FooterModule {}
