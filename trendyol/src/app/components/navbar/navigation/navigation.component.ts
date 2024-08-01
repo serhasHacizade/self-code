5013819becf73d1702f808e27296982cffd76552
serhasHacizade <eren.felek.4@gmail.com>
2024-08-01 13:06:17 +0300

    commit

--- trendyol/src/app/components/navbar/navigation/navigation.component.ts
+++ trendyol/src/app/components/navbar/navigation/navigation.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IndexService } from '../../../services/index.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss',
+  styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   data: any;
+  hoveredIndex: number | null = null;
+  @ViewChild('#hover-container')
+  hoverContainer!: ElementRef;
 
   constructor(private httpService: IndexService) {}
 
   ngOnInit() {
     this.httpService.getNavigation().subscribe((response) => {
       this.data = response;
-    });    
-  }  
+    });
+  }
+
+  onMouseOver(index: number) {
+    this.hoveredIndex = index;
+    this.hoverContainer.nativeElement.style.display = "block"
+  }
+
+  onMouseLeave() {
+    this.hoveredIndex = null;
+  }
 }
